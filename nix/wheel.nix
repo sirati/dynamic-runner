cae56d83b552728228ea44dd3da47c5b01c443de
sirati <git@sirati.me>
2026-05-10 11:56:54 +0200

    nix: bump cargoDeps hash for tracing-test addition
    The SSH-master 18h-floor merge added tracing-test + tracing-test-macro as dev-dependencies and updated Cargo.lock; this bumps nix/wheel.nix's cargoDeps.hash accordingly, verified locally via nix build .#dynamic-runner.
    
    Signed-off-by: sirati <git@sirati.me>

--- nix/wheel.nix
+++ nix/wheel.nix
@@ -29,7 +29,7 @@ buildPythonPackage {
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     src = lib.cleanSource ./..;
-    hash = "sha256-+IRFCdOlrWhgCoNWitV9rxLBADXfFjFooEpr9mNYGyI=";
+    hash = "sha256-MJo1h5uTC+dmUMMyXAKEfU2FuFqbxW9T6xPzEJpeI5c=";
   };
 
   nativeBuildInputs = [
